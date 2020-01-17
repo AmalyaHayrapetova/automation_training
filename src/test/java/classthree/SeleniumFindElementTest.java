2e0f26809ec262cb34baf56e188938138587b3a4
Amalya Hayrapetova <amalya.hayrapetova@betconstruct.com>
2020-01-17 10:56:39 +0400

    added SeleniumFindElementTest and SeleniumOpenMethodTest

---
+++ src/test/java/classthree/SeleniumFindElementTest.java
@@ -0,0 +1,54 @@
+package classthree;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class SeleniumFindElementTest {
+
+    private WebDriver driver;
+
+    @Test
+    public void clickOnElement() {
+
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriverMac");
+
+        //init chrome driver
+        driver = new ChromeDriver();
+
+        //1 - maximize window
+        driver.manage().window().maximize();
+
+
+        //open url
+        driver.get("https://the-internet.herokuapp.com/");
+
+        //find element by link text and get its text
+
+        String linkText = driver.findElement(By.linkText("Digest Authentication")).getText();
+        System.out.println("The elements text is: " + linkText);
+
+        /*
+        //exception while fining element
+        String exceptionText = driver.findElement(By.linkText("Automation")).getText();
+        System.out.println("The elements text is: " + exceptionText);
+        */
+
+
+        //find elements and get its size
+        List<WebElement> list = driver.findElements(By.tagName("a"));
+        System.out.println("The list size with \"a\" tag name is: " + list.size());
+
+
+        //close(quite) driver
+        driver.quit();
+
+
+    }
+
+
+}
---
+++ src/test/java/classthree/SeleniumOpenMethodTest.java
@@ -0,0 +1,114 @@
+package classthree;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumOpenMethodTest {
+
+    private WebDriver driver;
+
+
+    @Test
+    public void openPageWithMaximizedWind() {
+        /**
+         * Inside of here, Selenium WebDriver is going to need to know where is that executable file that you have,
+         * because I need it in order to run. We do this using a System.setProperty() and we give this the name of the property.
+         * And this property that Selenium will look for is called webdriver.chrome.driver,
+         * and it must be specified exactly like this because this is what Selenium will look for. This is like a key.
+         */
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriverMac");
+
+        //init chrome driver
+        driver = new ChromeDriver();
+
+        //1 - maximize window
+        driver.manage().window().maximize();
+
+        /*
+        //2 - full screen mode
+        driver.manage().window().fullscreen();
+         */
+
+        /*
+        //3 - Specific width (show Chrome iPhoneX emulator)
+        Dimension size = new Dimension(375, 812);
+        driver.manage().window().setSize(size);
+
+         */
+
+        //open url
+        driver.get("https://the-internet.herokuapp.com/");
+
+        //print page title
+        System.out.println(driver.getTitle());
+
+        //close(quite) driver
+        driver.quit();
+
+
+    }
+
+
+    @Test
+    public void openPageAsMobileWind() {
+        /**
+         * Inside of here, Selenium WebDriver is going to need to know where is that executable file that you have,
+         * because I need it in order to run. We do this using a System.setProperty() and we give this the name of the property.
+         * And this property that Selenium will look for is called webdriver.chrome.driver,
+         * and it must be specified exactly like this because this is what Selenium will look for. This is like a key.
+         */
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriverMac");
+
+        //init chrome driver
+        driver = new ChromeDriver();
+
+
+        //3 - Specific width (show Chrome iPhoneX emulator)
+        Dimension size = new Dimension(375, 812);
+        driver.manage().window().setSize(size);
+
+        //open url
+        driver.get("https://the-internet.herokuapp.com/");
+
+        //print page title
+        System.out.println(driver.getTitle());
+
+        //close(quite) driver
+        driver.quit();
+
+    }
+
+
+    @Test
+    public void openPageAsFullScreen() {
+        /**
+         * Inside of here, Selenium WebDriver is going to need to know where is that executable file that you have,
+         * because I need it in order to run. We do this using a System.setProperty() and we give this the name of the property.
+         * And this property that Selenium will look for is called webdriver.chrome.driver,
+         * and it must be specified exactly like this because this is what Selenium will look for. This is like a key.
+         */
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriverMac");
+
+        //init chrome driver
+        driver = new ChromeDriver();
+
+
+        //2 - full screen mode
+        driver.manage().window().fullscreen();
+
+
+        //open url
+        driver.get("https://the-internet.herokuapp.com/");
+
+        //print page title
+        System.out.println(driver.getTitle());
+
+        //close(quite) driver
+        driver.quit();
+
+    }
+
+
+}
